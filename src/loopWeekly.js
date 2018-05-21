5a89cf062225f562a60cffe3c1c3317d97b9d3cf
Jason Ashley <jason@tradingbills.com>
2018-05-21 14:24:09 -0700

    reworded variables for better understanding

--- src/loopWeekly.js
+++ src/loopWeekly.js
@@ -6,28 +6,28 @@ let startDateArg = "2018-01-01"
 let endDateArg = "2018-01-14"
 
 // get and set entered time
-let wholeDateRange = new DateRange();
-wholeDateRange.DateRange= {
+let enteredDateRange = new DateRange();
+enteredDateRange.DateRange= {
 	lowerUTC: startDateArg,
 	upperUTC: endDateArg
 }
-enteredDateRange = wholeDateRange.DateRange;
+dateRange = enteredDateRange.DateRange;
 
 // display days and weeks (note add +1 to include a full day(s)
-console.log('Total full days', moment(enteredDateRange.upperUTC).utc().diff(moment(enteredDateRange.lowerUTC).utc(), 'days') + 1)
-let totalWeeks = Math.floor((moment(enteredDateRange.upperUTC).utc().diff(moment(enteredDateRange.lowerUTC).utc(), 'days') + 1) / 7)
+console.log('Total full days', moment(dateRange.upperUTC).utc().diff(moment(dateRange.lowerUTC).utc(), 'days') + 1)
+let totalWeeks = Math.floor((moment(dateRange.upperUTC).utc().diff(moment(dateRange.lowerUTC).utc(), 'days') + 1) / 7)
 console.log('Total Weeks ' + totalWeeks)
 // display start and end dates, after util calculates sod, StartOfDay, and eod, EndOfDay, + show as EPOCH
-console.log('\n', moment(timeUtils.UTCMakeGMT0(enteredDateRange.lowerUTC, 'sod')).utc().format('LLL'), moment(timeUtils.UTCMakeGMT0(enteredDateRange.lowerUTC, 'sod')).utc().format('X'))
-console.log('', moment(timeUtils.UTCMakeGMT0(enteredDateRange.upperUTC, 'eod')).utc().format('LLL'), moment(timeUtils.UTCMakeGMT0(enteredDateRange.upperUTC, 'eod')).utc().format('X'))
+console.log('\n', moment(timeUtils.UTCMakeGMT0(dateRange.lowerUTC, 'sod')).utc().format('LLL'), moment(timeUtils.UTCMakeGMT0(dateRange.lowerUTC, 'sod')).utc().format('X'))
+console.log('', moment(timeUtils.UTCMakeGMT0(dateRange.upperUTC, 'eod')).utc().format('LLL'), moment(timeUtils.UTCMakeGMT0(dateRange.upperUTC, 'eod')).utc().format('X'))
 
 // Outer loop will be of Total Range on a weekly loop
 //TODO: handle (( ( thisEndDay.diff(thisStartDay) % 7 )==0 ? true : false ) ) to finish a segment < a full 7 days
 for (var x = 0; x < totalWeeks; x++) {
 	console.log('Outer Loop week ' + (x + 1).toString());
 	// display this weeks start and end dates based upon loop interation
-	let useStartDate = timeUtils.UTCMakeGMT0(moment(enteredDateRange.lowerUTC).utc().add((x * 7), 'days').format('YYYY-MM-DD'), 'sod');
-	let useEndDate = timeUtils.UTCMakeGMT0(moment(enteredDateRange.lowerUTC).utc().add((x * 7) + 6, 'days').format('YYYY-MM-DD'), 'eod');
+	let useStartDate = timeUtils.UTCMakeGMT0(moment(dateRange.lowerUTC).utc().add((x * 7), 'days').format('YYYY-MM-DD'), 'sod');
+	let useEndDate = timeUtils.UTCMakeGMT0(moment(dateRange.lowerUTC).utc().add((x * 7) + 6, 'days').format('YYYY-MM-DD'), 'eod');
 
 	console.log(`\t  usedStartDate: ${moment(useStartDate).utc().format('LLL')} , useEndDate: ${moment(useEndDate).utc().format('LLL')}`);
 	// loop through the week's days
