76418173d9b11ddbf620efc501aa8cb3418d4996
Jason Ashley <jason@tradingbills.com>
2018-05-23 08:19:36 -0700

    text change to distinguish _dateRange as a private, locally scoped variable

--- src/class/DateRange.js
+++ src/class/DateRange.js
@@ -1,13 +1,13 @@
 const moment = require('moment')
 const timeUtil = require('../utils/timeUtils')
 
-function DateRange(givenRange) {
+function DateRange() {
 
-  let dateRange = {}
+  let _dateRange = {lowerUTC:"", upperUTC:""}
 
   Object.defineProperty(this, 'dateRange', {
       get: () => {
-        return dateRange
+        return _dateRange
       },
       set: (newRange) => {
         if (newRange === null || newRange === undefined) {
@@ -15,32 +15,31 @@ function DateRange(givenRange) {
         }
 
         // lowerUTC gaurds
-        // upper and lower don't exist, set lower and upper
         if ((newRange.lowerUTC === null || newRange.lowerUTC === undefined) &&
           (newRange.upperUTC === null || newRange.upperUTC === undefined)
         ) {
-          dateRange.lowerUTC = moment().utc().format('YYYY-MM-DD');
-          dateRange.upperUTC = moment().utc().format('YYYY-MM-DD');
+          _dateRange.lowerUTC = moment().utc().format('YYYY-MM-DD');
+          _dateRange.upperUTC = moment().utc().format('YYYY-MM-DD');
         } else if (
           (newRange.lowerUTC === null || newRange.lowerUTC === undefined) &&
           (newRange.upperUTC !== null)
         ) {
-          dateRange.lowerUTC = moment(newRange.upperUTC).utc().format('YYYY-MM-DD')
+          _dateRange.lowerUTC = moment(newRange.upperUTC).utc().format('YYYY-MM-DD')
         } else if (newRange.lowerUTC !== null) {
-          dateRange.lowerUTC = moment(newRange.lowerUTC).utc().format('YYYY-MM-DD');
+          _dateRange.lowerUTC = moment(newRange.lowerUTC).utc().format('YYYY-MM-DD');
         }
         // uppUTC gaurds
         if ((newRange.lowerUTC === null || newRange.lowerUTC === undefined) &&
           (newRange.upperUTC === null || newRange.upperUTC === undefined)
         ) {
-          dateRange.upperUTC = moment().utc().format('YYYY-MM-DD')
+          _dateRange.upperUTC = moment().utc().format('YYYY-MM-DD')
         } else if (
           (newRange.upperUTC === null || newRange.upperUTC === undefined) &&
           (newRange.lowerUTC !== null)
         ) {
-          dateRange.upperUTC = moment(newRange.lowerUTC).utc().format('YYYY-MM-DD')
+          _dateRange.upperUTC = moment(newRange.lowerUTC).utc().format('YYYY-MM-DD')
         } else if (newRange.upperUTC !== null) {
-          dateRange.upperUTC = moment(newRange.upperUTC).utc().format('YYYY-MM-DD');
+          _dateRange.upperUTC = moment(newRange.upperUTC).utc().format('YYYY-MM-DD');
         }
 
       }
